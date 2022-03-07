92ecb9224c743e5eeb1402caef7e0b9de33a9a09
Henrry Hernandez <jenrruhndz@gmail.com>
2022-03-06 18:54:48 -0700

    style (auth): add mock up for login and register views

--- src/containers/auth/login/Login.tsx
+++ src/containers/auth/login/Login.tsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
+
 export const Login = () => {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    console.log("submitting...");
+  };
+
   return (
-    <>
-      <p>Login</p>
-    </>
+    <div className="auth">
+      <div className="auth__inner_box">
+        <h2 className="auth__title">Sign in</h2>
+
+        <form className="auth__form" onSubmit={handleSubmit}>
+          <input
+            className="auth__input"
+            type="text"
+            placeholder="Username"
+            required
+          />
+          <input
+            className="auth__input"
+            type="password"
+            placeholder="Password"
+            required
+          />
+
+          <input
+            className="auth__input auth__input-button"
+            type="submit"
+            value="Login"
+          />
+        </form>
+
+        <hr />
+
+        <Link className="btn auth__link" to="/auth/register">
+          Create new
+        </Link>
+      </div>
+    </div>
   );
 };
--- src/containers/auth/register/Register.tsx
+++ src/containers/auth/register/Register.tsx
@@ -1,7 +1,63 @@
+import { Link } from "react-router-dom";
+
 export const Register = () => {
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    console.log("submitting...");
+  };
+
   return (
-    <>
-      <p>Register</p>
-    </>
+    <div className="auth">
+      <div className="auth__inner_box">
+        <h2 className="auth__title">Sign up</h2>
+
+        <form className="auth__form" onSubmit={handleSubmit}>
+          <input
+            className="auth__input"
+            type="text"
+            placeholder="Name"
+            required
+          />
+          <input
+            className="auth__input"
+            type="text"
+            placeholder="Lastname 1"
+            required
+          />
+          <input className="auth__input" type="text" placeholder="Lastname 2" />
+          <input
+            className="auth__input"
+            type="text"
+            placeholder="Username"
+            required
+          />
+          <input
+            className="auth__input"
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <input
+            className="auth__input"
+            type="password"
+            placeholder="Confirm password"
+            required
+          />
+
+          <input
+            className="auth__input auth__input-button"
+            type="submit"
+            value="Login"
+          />
+        </form>
+
+        <hr />
+
+        <Link className="btn auth__link" to="/auth/login">
+          Already have one
+        </Link>
+      </div>
+    </div>
   );
 };
