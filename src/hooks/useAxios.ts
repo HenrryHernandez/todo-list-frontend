959614758e89cb35c06bf16000bfa109751334c3
Henrry Hernandez <jenrruhndz@gmail.com>
2022-03-08 12:24:28 -0700

    refactor (useAxios): change variables location

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -4,10 +4,10 @@ import { axiosInstance } from "../api/apiInstance";
 
 export const useAxios = () => {
   useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken") || "";
-
     const requestInterceptor = axiosInstance.interceptors.request.use(
       (config: any) => {
+        const accessToken = localStorage.getItem("accessToken") || "";
+
         if (!config.headers.Authorization) {
           config.headers.Authorization = `Bearer ${accessToken}`;
         }
@@ -24,11 +24,16 @@ export const useAxios = () => {
         return response;
       },
       async (error) => {
-        const MAX_TRIES = 3;
         const config = error?.config;
+        const response = error?.response;
+
+        if (response?.status === 403) {
+          console.log("logout");
+        }
 
         config.__retryCount = config.__retryCount || 0;
 
+        const MAX_TRIES = 3;
         if (config.__retryCount >= MAX_TRIES) return Promise.reject(error);
 
         config.__retryCount++;
