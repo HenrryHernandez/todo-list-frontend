607526b0f6befd7f870d7a1d4eaf82f41d7315ee
Henrry Hernandez <jenrruhndz@gmail.com>
2022-03-08 18:47:28 -0700

    feat (MainRouter): add check token functionality for route protection

--- src/routers/MainRouter.tsx
+++ src/routers/MainRouter.tsx
@@ -1,3 +1,4 @@
+import { useContext, useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { PrivateRouter } from "./PrivateRoute";
@@ -6,15 +7,33 @@ import { PublicRouter } from "./PublicRoute";
 import { AuthRouter } from "../containers/auth/AuthRouter";
 import { Dashboard } from "../containers/dashboard/Dashboard";
 
+import { TokenContext } from "../contexts/TokenContext";
+
 export const MainRouter = () => {
-  const allowNav = true;
+  const { token } = useContext(TokenContext);
+
+  const [thereIsToken, setThereIsToken] = useState(false);
+  const [confirmingToken, setConfirmingToken] = useState(true);
+
+  useEffect(() => {
+    setThereIsToken(token !== null);
+  }, [token]);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    setThereIsToken(accessToken !== null);
+    setConfirmingToken(false);
+  }, []);
+
+  if (confirmingToken) return <div>Loading...</div>;
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<PrivateRouter allowNavigation={allowNav} />}>
+        <Route element={<PrivateRouter allowNavigation={thereIsToken} />}>
           <Route path="/" element={<Dashboard />}></Route>
         </Route>
-        <Route element={<PublicRouter allowNavigation={!allowNav} />}>
+        <Route element={<PublicRouter allowNavigation={!thereIsToken} />}>
           <Route path="/auth/*" element={<AuthRouter />}></Route>
         </Route>
 
