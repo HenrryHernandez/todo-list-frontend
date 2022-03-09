cd6bd50e9909d177bcc79abe19a25ab817f95761
Henrry Hernandez <jenrruhndz@gmail.com>
2022-03-08 19:51:36 -0700

    feat (Login): add login functionality

--- src/containers/auth/login/Login.tsx
+++ src/containers/auth/login/Login.tsx
@@ -1,11 +1,50 @@
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
+import { TokenContext } from "../../../contexts/TokenContext";
+import { UserContext } from "../../../contexts/UserContext";
+
+import { useAxios } from "./../../../hooks/useAxios";
+
+import { In_LoginResponse } from "../../../interfaces/Auth.interface";
+
 export const Login = () => {
-  const handleSubmit = (e: any) => {
+  const axiosInstance = useAxios();
+
+  const [username, setUsername] = useState("user_two2");
+  const [password, setPassword] = useState("o12345678.");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { saveUser } = useContext(UserContext);
+  const { setToken } = useContext(TokenContext);
+
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    console.log("submitting...");
+    setIsLoading(true);
+
+    try {
+      const response = await axiosInstance.post<In_LoginResponse>(
+        "/api/auth/login",
+        {
+          username,
+          password,
+        }
+      );
+
+      localStorage.setItem("accessToken", response.data.token);
+
+      saveUser(response.data.user);
+
+      setToken(response.data.token);
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
   };
+
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className="auth">
@@ -17,12 +56,16 @@ export const Login = () => {
             className="auth__input"
             type="text"
             placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             required
           />
           <input
             className="auth__input"
             type="password"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             required
           />
 
