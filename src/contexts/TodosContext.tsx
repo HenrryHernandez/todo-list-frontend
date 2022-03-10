a8c708a03f70a8b1bde3f0722e16f7ca2fe4213a
Henrry Hernandez <jenrruhndz@gmail.com>
2022-03-10 12:20:00 -0700

    feat (TodosContext): add set current todo functionality

--- src/contexts/TodosContext.tsx
+++ src/contexts/TodosContext.tsx
@@ -5,15 +5,25 @@ import { ITodo } from "../interfaces/Todo.interface";
 type TodosContextProps = {
   todos: ITodo[];
   setTodos: React.Dispatch<React.SetStateAction<ITodo[]>>;
+  currentTodo: ITodo | null;
+  setCurrentTodo: React.Dispatch<React.SetStateAction<ITodo | null>>;
+  selectTodoById: (todoId: number) => void;
 };
 
 export const TodosContext = createContext({} as TodosContextProps);
 
 export const TodosProvider = ({ children }: any) => {
   const [todos, setTodos] = useState<ITodo[]>([]);
+  const [currentTodo, setCurrentTodo] = useState<ITodo | null>(null);
+
+  const selectTodoById = (todoId: number) => {
+    setCurrentTodo(todos.find((el) => el.id === todoId) ?? null);
+  };
 
   return (
-    <TodosContext.Provider value={{ todos, setTodos }}>
+    <TodosContext.Provider
+      value={{ todos, setTodos, currentTodo, setCurrentTodo, selectTodoById }}
+    >
       {children}
     </TodosContext.Provider>
   );
